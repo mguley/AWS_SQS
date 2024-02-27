f27f6091033ad7b4466194bcc4f94746bfaf00a2
Mykola Guley <mykolaguley99@gmail.com>
2024-02-28 00:31:27 +0200

    added DAL (data access layer) logic (prototype)

---
+++ Customers.Api/Data/DataExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Customers.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Customers.Api.Data;
+
+/// <summary>
+/// Provides extension methods for initializing and migrating the database.
+/// </summary>
+public static class DataExtensions
+{
+    /// <summary>
+    /// Applies any pending migrations for the context to the database.
+    /// Will create the database if it does not already exist.
+    /// </summary>
+    /// <param name="app">The web application to which the database migration is applied.</param>
+    /// <returns>A task representing the asynchronous operation of the database migration.</returns>
+    public static async Task MigrateDbAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<CustomersContext>();
+        await dbContext.Database.MigrateAsync();
+    }
+}
--- Customers.Api/Program.cs
+++ Customers.Api/Program.cs
@@ -1,9 +1,19 @@
+using Customers.Api.Data;
 using Customers.Api.Extensions;
+using Customers.DAL.Abstractions;
+using Customers.DAL.Data;
+using Customers.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpoints(typeof(Program).Assembly);
 
+var connString = builder.Configuration.GetConnectionString(name: "Customers");
+builder.Services.AddSqlite<CustomersContext>(connectionString: connString);
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
 var app = builder.Build();
 
 app.MapEndpoints();
+await app.MigrateDbAsync();
+
 app.Run();
