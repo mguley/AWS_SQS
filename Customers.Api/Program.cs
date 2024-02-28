32ba3641cae7b6e8d28ebf6d2f3ff73e42b7d19e
Mykola Guley <mykolaguley99@gmail.com>
2024-02-28 14:22:36 +0200

    DAL - added initialization data for Customers

--- Customers.Api/Program.cs
+++ Customers.Api/Program.cs
@@ -2,14 +2,18 @@ using Customers.Api.Data;
 using Customers.Api.Extensions;
 using Customers.DAL.Abstractions;
 using Customers.DAL.Data;
+using Customers.DAL.Data.InitDataFactory;
 using Customers.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpoints(typeof(Program).Assembly);
 
 var connString = builder.Configuration.GetConnectionString(name: "Customers");
+
+// DAL dependencies
 builder.Services.AddSqlite<CustomersContext>(connectionString: connString);
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddSingleton<AbstractDataFactory, DataFactory>();
 
 var app = builder.Build();
 
---
+++ Customers.DAL/Data/InitDataFactory/DataFactory.cs
@@ -0,0 +1,38 @@
+﻿using Customers.DAL.Entities;
+
+namespace Customers.DAL.Data.InitDataFactory;
+
+/// <summary>
+/// A concrete implementation of <see cref="AbstractDataFactory"/> that provides initial customer data.
+/// </summary>
+public class DataFactory : AbstractDataFactory
+{
+    /// <summary>
+    /// Provides an array of predefined <see cref="Customer"/> objects to seed the database.
+    /// </summary>
+    /// <returns>An array of <see cref="Customer"/> objects with predefined initial data.</returns>
+    public override Customer[] GetCustomersInitialData()
+    {
+        return new Customer[]
+        {
+            new()
+            {
+                FullName = "John Doe",
+                Email = "john.doe@example.com",
+                GitHubUsername = "johndoe123",
+            },
+            new()
+            {
+                FullName = "Jane Smith",
+                Email = "jane.smith@example.com",
+                GitHubUsername = "janesmith456",
+            },
+            new()
+            {
+                FullName = "Bob Johnson",
+                Email = "bob.johnson@example.com",
+                GitHubUsername = "bobjohnson789"
+            }
+        };
+    }
+}
